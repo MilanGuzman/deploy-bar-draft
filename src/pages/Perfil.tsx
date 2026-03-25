94e530569d6c5e5e8f12d8cbe23cd9af264b66cf
DiegoCF8377 <a00835977@tec.mx>
2026-03-25 10:51:12 -0600

    Container Datos del user

--- src/pages/Perfil.tsx
+++ src/pages/Perfil.tsx
@@ -1,14 +1,27 @@
-
+import PerfilUsuario from "../components/perfil/PerfilUsuario";
 
 
 function Perfil() {
 
   return (
-    <div className="flex flex-col items-center justify-center h-screen gap-6 bg-[#002244]">
-      <img className="h-12 w-19 object-contain" src="/barcaLogo.png" />
-      <h1 className='text-3xl font-extrabold text-white'>Barca Perfil</h1>
+    <div className="p-6 bg-[#002244] min-h-screen">
+      
+      <PerfilUsuario
+        username="pedroTrej5"
+        ranking={14}
+        pais="Chile"
+        avatarUrl="https://i.pravatar.cc/150?img=12"
+        onLogout={() => alert("Tu no mete cabra")}        
+        puntos={2400}
+        logros={3}
+        predicciones={4}
+        nivel={1}
+        xpActual={2}
+        xpMax={4000}
+      />
     </div>
-  )
+  );
+
 }
 
 export default Perfil

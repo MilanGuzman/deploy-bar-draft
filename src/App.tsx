377d7f8db75dd6a410453374ba15dc5a77d35ece
German-coder07 <gercuetol@gmail.com>
2026-03-30 14:53:28 -0600

    create wpHUB

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,7 @@ import SportsApi from "./pages/SportsApi"
 import Layout from "./layouts/Layout"
 import Index from "./pages/Index"
 import Chat from "./pages/Chat"
-import WatchParty from "./pages/WatchParty";
+import WatchParty from "./pages/WatchPartyHUB.tsx";
 import Wordle from "./pages/Wordle"
 import Ra from "./pages/ra"
 import Reels from "./pages/Reels"
@@ -15,7 +15,7 @@ function App() {
         <Route path="/" element={<Index />} />
         <Route path="/sportsapi" element={<SportsApi />} />
         <Route path="/chat" element={<Chat />} />
-        <Route path="/WatchParty" element={<WatchParty />} />
+        <Route path="/watchparty" element={<WatchParty />} />
         <Route path="/wordle" element={<Wordle />} />
         <Route path="/ra" element={<Ra />} />
         <Route path="/reels" element={<Reels />} />

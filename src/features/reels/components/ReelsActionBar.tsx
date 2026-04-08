a79d505df52c27928eee0abdb374c215a185e2b7
Daniel <danielara071@gmail.com>
2026-04-07 18:22:54 -0600

    like visual functionality

--- src/features/reels/components/ReelsActionBar.tsx
+++ src/features/reels/components/ReelsActionBar.tsx
@@ -1,16 +1,23 @@
 import { Ellipsis, Heart, Share } from "lucide-react";
+import { useState } from "react";
+
+
+const crimsonColor = "#B5174B";
 
 const ReelsActionBar = () => {
+
+  const [liked, setLiked] = useState(false)
+
   return (
     <div className="bg-brand-white h-12 w-60 rounded-4xl flex flex-row items-center justify-center gap-x-8 ">
-      <button className="cursor-pointer h-full w-7 flex items-center justify-center max-h-full">
-        <Heart color="#B5174B"  className=""/>
+      <button className="cursor-pointer h-full w-7 flex items-center justify-center max-h-full" onClick={() => setLiked((prev) => !prev )}>
+        <Heart color={crimsonColor}  fill={liked ? crimsonColor : "none"} />
       </button>
       <button className="cursor-pointer h-7 w-7 flex items-center justify-center">
-        <Share color="#B5174B" />
+        <Share color={crimsonColor} />
       </button>
       <button className="cursor-pointer h-7 w-7 flex items-center justify-center">
-        <Ellipsis color="#B5174B" />
+        <Ellipsis color={crimsonColor} />
       </button>
     </div>
   );

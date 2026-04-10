f12d3e10846c8bf73832fedd4e8a0e8e145a3726
Daniel <danielara071@gmail.com>
2026-04-09 22:18:21 -0600

    refactor reel card logic into card component

---
+++ src/features/reels/components/ReelCard.tsx
@@ -0,0 +1,143 @@
+import { VolumeX, Volume2, Play } from "lucide-react";
+import { motion, type Transition } from "motion/react";
+import { i } from "motion/react-client";
+import { useState } from "react";
+
+const spring: Transition = {
+  type: "spring",
+  stiffness: 100,
+  damping: 22,
+  mass: 0.8,
+};
+
+interface ReelCardProps {
+  id: string;
+  video_url: string;
+  thumbnail_url: string;
+  caption: string;
+  duration?: number | null;
+  animateState: string;
+  position: string;
+  changeActiveIndex: () => void;
+}
+
+const ReelCard = ({
+  id,
+  video_url,
+  thumbnail_url,
+  caption,
+  duration,
+  animateState,
+  position,
+  changeActiveIndex
+
+}: ReelCardProps) => {
+  const variants = {
+    center: {
+      scale: 1,
+      x: 0,
+      opacity: 1,
+      zIndex: 3,
+      width: 384, // w-96
+      height: 620,
+      rotate: 0,
+    },
+    left: {
+      scale: 1,
+      x: -280,
+      opacity: 0.45,
+      zIndex: 2,
+      width: 224, // w-56
+      height: 288, // h-72
+      rotate: -5,
+    },
+    right: {
+      scale: 1,
+      x: 280,
+      opacity: 0.45,
+      zIndex: 2,
+      width: 224,
+      height: 288,
+      rotate: 5,
+    },
+    // Hidden cards are placed offscreen left or right so they slide IN,
+    // not pop in from the center
+    hiddenLeft: {
+      scale: 0.8,
+      x: -560,
+      opacity: 0,
+      zIndex: 0,
+      width: 224,
+      height: 288,
+      rotate: -5,
+    },
+    hiddenRight: {
+      scale: 0.8,
+      x: 560,
+      opacity: 0,
+      zIndex: 0,
+      width: 224,
+      height: 288,
+      rotate: 5,
+    },
+  };
+  const [muted, setMuted] = useState(true);
+
+  return (
+    <motion.div
+      key={id}
+      variants={variants}
+      animate={animateState}
+      transition={spring}
+      className="absolute overflow-hidden rounded-2xl shadow-2xl shadow-black group"
+      onClick={changeActiveIndex}
+    >
+      {/* TODO: make whole motion div clickable as the pause button*/}
+      {/* <img
+                  src={reel.thumbnail_url}
+                  alt={reel.caption}
+                  className="w-full h-full object-cover"
+                /> */}
+      <video
+        src={video_url}
+        autoPlay
+        muted
+        loop
+        playsInline
+        controls
+        controlsList="nofullscreen"
+        style={{
+          width: "100%",
+          height: "100vh",
+          objectFit: "cover",
+          outline: "none",
+        }}
+      />
+      <button
+        className="absolute top-6 left-6 cursor-pointer"
+        onClick={() => setMuted((prev) => !prev)}
+      >
+        {muted ? (
+          <VolumeX color="white" fill="white" className="w-8 h-8" />
+        ) : (
+          <Volume2 color="white" fill="white" className="w-8 h-8" />
+        )}
+      </button>
+      <div className="absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 opacity-0 group-hover:opacity-100 transition duration-300 ">
+        <Play className="w-8 h-8 text-white fill-white" />
+      </div>
+      <div className="absolute inset-0 bg-linear-to-t from-black/90 via-transparent to-transparent pointer-events-none" />
+
+      {/* Title — fades in only for center */}
+      <motion.div
+        className="absolute bottom-0 left-0 right-0 p-4"
+        animate={{ opacity: position === "center" ? 1 : 0 }}
+        transition={{ duration: 0.25 }}
+      >
+        <p className="text-white text-sm font-semibold mb-3">{caption}</p>
+      </motion.div>
+    </motion.div>
+  );
+};
+
+export default ReelCard;
--- src/features/reels/components/ReelsFeed.tsx
+++ src/features/reels/components/ReelsFeed.tsx
@@ -1,5 +1,5 @@
 // ReelsSection.tsx
-import { use, useEffect, useState } from "react";
+import { useEffect, useState } from "react";
 import reels1 from "@/data/img/reels1.jpg";
 import reels2 from "@/data/img/reels2.jpg";
 import reels3 from "@/data/img/reels3.jpg";
@@ -9,6 +9,7 @@ import { motion, type Transition } from "motion/react";
 import { fetchReels } from "../services/supabase";
 import type { VideoReel } from "../interfaces/VideoReel";
 import { Play, Volume2, VolumeX } from "lucide-react";
+import ReelCard from "./ReelCard";
 
 const reels = [
   { id: 1, title: "El Barcelona gana contra Levante 3-1", thumbnail: reels1 },
@@ -17,67 +18,10 @@ const reels = [
   { id: 4, title: "Entrenamiento Camp Nou", thumbnail: reels4 },
 ];
 
-const spring: Transition = {
-  type: "spring",
-  stiffness: 100,
-  damping: 22,
-  mass: 0.8,
-};
-
 const ReelsFeed = () => {
-  const variants = {
-    center: {
-      scale: 1,
-      x: 0,
-      opacity: 1,
-      zIndex: 3,
-      width: 384, // w-96
-      height: 620,
-      rotate: 0,
-    },
-    left: {
-      scale: 1,
-      x: -280,
-      opacity: 0.45,
-      zIndex: 2,
-      width: 224, // w-56
-      height: 288, // h-72
-      rotate: -5,
-    },
-    right: {
-      scale: 1,
-      x: 280,
-      opacity: 0.45,
-      zIndex: 2,
-      width: 224,
-      height: 288,
-      rotate: 5,
-    },
-    // Hidden cards are placed offscreen left or right so they slide IN,
-    // not pop in from the center
-    hiddenLeft: {
-      scale: 0.8,
-      x: -560,
-      opacity: 0,
-      zIndex: 0,
-      width: 224,
-      height: 288,
-      rotate: -5,
-    },
-    hiddenRight: {
-      scale: 0.8,
-      x: 560,
-      opacity: 0,
-      zIndex: 0,
-      width: 224,
-      height: 288,
-      rotate: 5,
-    },
-  };
   // right now returns promise
 
   const [activeIndex, setActiveIndex] = useState(0);
-  const [muted, setMuted] = useState(true);
 
   const getPosition = (i: number) => {
     const diff = i - activeIndex;
@@ -97,7 +41,7 @@ const ReelsFeed = () => {
   type Key = "ArrowLeft" | "ArrowRight";
   const keyHandlers: Record<Key, () => void> = {
     ArrowLeft: () => setActiveIndex((i) => Math.max(i - 1, 0)),
-    ArrowRight: () => setActiveIndex((i) => Math.min(i + 1, reels.length - 1)),
+    ArrowRight: () => setActiveIndex((i) => Math.min(i + 1, videos.length - 1)),
   };
 
   const handleKeyDown = (e: React.KeyboardEvent) => {
@@ -129,6 +73,10 @@ const ReelsFeed = () => {
     console.log(videos);
   }, [videos]);
 
+  useEffect(() => {
+    console.log("updateando :  ",activeIndex);
+  }, [activeIndex]);
+
   return (
     <div
       tabIndex={0}
@@ -146,46 +94,15 @@ const ReelsFeed = () => {
             const animateState = getAnimateState(i);
 
             return (
-              
-              <motion.div
-                key={reel.id}
-                variants={variants}
-                animate={animateState}
-                transition={spring}
-                className="absolute overflow-hidden rounded-2xl shadow-2xl shadow-black group"
-                onClick={() => setActiveIndex(i)}
-              >
-                {/* TODO: make whole motion div clickable as the pause button*/}
-                <img
-                  src={reel.thumbnail_url}
-                  alt={reel.caption}
-                  className="w-full h-full object-cover"
-                />
-                <button className="absolute top-6 left-6 cursor-pointer" onClick={() => setMuted((prev) => !prev)}>
-                  {
-                    muted ?
-                    <VolumeX color="white" fill="white" className="w-8 h-8" />
-                    :
-                    <Volume2 color="white" fill="white" className="w-8 h-8" />
-                    
-                  }
-                </button>
-                <div className="absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 opacity-0 group-hover:opacity-100 transition duration-300 ">
-                  <Play className="w-8 h-8 text-white fill-white" />
-                </div>
-                <div className="absolute inset-0 bg-linear-to-t from-black/90 via-transparent to-transparent pointer-events-none" />
-                  
-                {/* Title — fades in only for center */}
-                <motion.div
-                  className="absolute bottom-0 left-0 right-0 p-4"
-                  animate={{ opacity: position === "center" ? 1 : 0 }}
-                  transition={{ duration: 0.25 }}
-                >
-                  <p className="text-white text-sm font-semibold mb-3">
-                    {reel.caption}
-                  </p>
-                </motion.div>
-              </motion.div>
+              <ReelCard
+                id={reel.id}
+                video_url={reel.video_url}
+                thumbnail_url={reel.thumbnail_url}
+                caption={reel.caption}
+                animateState={animateState}
+                position={position}
+                changeActiveIndex={() => setActiveIndex(i)}
+              />
             );
           })}
         </div>
